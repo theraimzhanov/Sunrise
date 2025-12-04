0c6ecad1d4754e966aa1dcd3e735b47a0b4aa7aa
theraimzhanov <raimjanovnursultan@gmail.com>
2025-12-03 19:24:19 -0600

    class from Json

---
+++ app/src/main/java/com/example/sunrise/model/Item0.kt
@@ -0,0 +1,18 @@
+package com.example.sunrise.model
+
+data class Item0(
+    val clouds: Int,
+    val deg: Int,
+    val dt: Int,
+    val feels_like: FeelsLike,
+    val gust: Double,
+    val humidity: Int,
+    val pop: Int,
+    val pressure: Int,
+    val snow: Double,
+    val speed: Double,
+    val sunrise: Int,
+    val sunset: Int,
+    val temp: Temp,
+    val weather: List<WeatherX>
+)
