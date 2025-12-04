db9be6a42f06066916defa9a7a569932d0a28efe
theraimzhanov <raimjanovnursultan@gmail.com>
2025-12-03 22:24:02 -0600

    repository and main viewModel

--- app/src/main/java/com/example/sunrise/di/AppModule.kt
+++ app/src/main/java/com/example/sunrise/di/AppModule.kt
@@ -1,4 +1,23 @@
 package com.example.sunrise.di
 
+import com.example.sunrise.network.WeatherApi
+import com.example.sunrise.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi() : WeatherApi{
+        return Retrofit.Builder().baseUrl(BASE_URL).
+                addConverterFactory(GsonConverterFactory.create())
+            .build().create(WeatherApi::class.java)
+    }
 }
