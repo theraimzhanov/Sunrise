b1861d46119e3163e73c13f0edb93c090a5385b6
theraimzhanov <raimjanovnursultan@gmail.com>
2025-12-05 01:02:04 -0600

    cicle image

--- app/src/main/java/com/example/sunrise/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/sunrise/screens/main/MainScreen.kt
@@ -1,27 +1,45 @@
 package com.example.sunrise.screens.main
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.sunrise.R
 import com.example.sunrise.data.DataOrException
 import com.example.sunrise.model.Weather
+import com.example.sunrise.utils.formatDate
+import com.example.sunrise.utils.formatDecimals
 import com.example.sunrise.widgets.WeatherAppBar
+import java.nio.file.WatchEvent
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel= hiltViewModel()) {
 
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)) {
-        value = mainViewModel.getWeatherData("London")
+        value = mainViewModel.getWeatherData("Moscow")
     }.value
 
     if (weatherData.loading ==true){
@@ -38,13 +56,45 @@ fun MainScaffold(weather: Weather,navController: NavController) {
         ,navController = navController, elevation = 1.dp){
 
     }}){
-        MainContent(data = weather)
+        MainContent(data = weather, modifier = Modifier.padding(it))
     }
 
 }
 
 
 @Composable
-fun MainContent(data: Weather){
-Text(data.list.toString())
+fun MainContent(data: Weather,modifier: Modifier){
+
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"
+
+    Column(modifier.padding(4.dp)
+        .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            formatDate(data.list[0].dt),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp))
+
+        Surface(modifier = Modifier.padding(4.dp).size(200.dp)
+            , shape = CircleShape, color = Color(0xFFFFC400)
+        ) {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(text = formatDecimals(data.list[0].temp.day)+"°", style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.ExtraBold)
+                Text(text =data.list[0].weather[0].main, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(painter = rememberImagePainter(  imageUrl),
+       contentDescription = "icon image",
+        modifier = Modifier.size(80.dp) )
 }
